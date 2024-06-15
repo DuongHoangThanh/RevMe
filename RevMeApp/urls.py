b122cbdf7d277388b1f157ac21c1cabdaad4b4b9
DHoangThanh1306 <dhoangthanh1306@gmail.com>
2024-06-15 22:45:41 +0700

    Add: show work and workout detail

--- RevMeApp/urls.py
+++ RevMeApp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import PredictObesity, AssessmentsList, AssessmentsDetail, GeneratePlanAPIView
+from .views import PredictObesity, AssessmentsList, AssessmentsDetail, GeneratePlanAPIView, HomePageView, WorkoutPlanView, DetailWorkoutView
 
 urlpatterns = [
     path('assessment/', AssessmentsList.as_view(), name='assessment-list'),
     path('assessment/<int:pk>/', AssessmentsDetail.as_view(), name='assessment-detail'),
     path('predict/', PredictObesity.as_view(), name='predict-obesity'),
     path('generate_plan/', GeneratePlanAPIView.as_view(), name='plan'),
+    path('home/', HomePageView.as_view(), name='home'),
+    path('workout/', WorkoutPlanView.as_view(), name='workout'),
+    path('workout/<int:pk>/', DetailWorkoutView.as_view(), name='workout-detail')
 ]
